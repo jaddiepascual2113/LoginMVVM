a39a8ad68d64c36d91261751878de6471a819c7d
Jaddie Allaine Pascual <jaddie.pascual@cimb.com>
2025-03-04 09:23:56 +0800

    Initial Commit

---
+++ LoginSimulate/LoginSimulateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSimulateApp.swift
+//  LoginSimulate
+//
+//  Created by ASCENDION on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginSimulateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
